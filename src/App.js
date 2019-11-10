d9220607c0f737215ee9b8794ace2e190875ed2c
pervezn <nidapervez2020@u.northwestern.edu>
2019-11-10 00:16:25 -0600

    some basic elements

--- src/App.js
+++ src/App.js
@@ -1,14 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
-import Button from '@material-ui/core/Button';
 import './App.css';
+import { Container, Typography, Box, Grid, GridListTile, GridList, Button } from '@material-ui/core';
+import { borders } from '@material-ui/system';
 
-function App() {
+const defaultProps = {
+  // bgcolor: 'background.paper',
+  // borderColor: 'text.Secondary',
+  // l: 500,
+  // // m:1,
+  // border: 1,
+  style: { width: '10rem', height: '10rem' },
+  // alignItems: "center"
+};
+
+const App = ()  => {
+  const [data, setData] = useState({});
+  const pictures = Object.values(data);
+  
+
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch('./pictures/pictures.json');
+      const json = await response.json();
+      setData(json);
+    };
+    fetchProducts();
+  }, []);
+
   return (
-    <Button variant="contained" color="primary">
-    Hello World
-  </Button>
+//className={classes.gridList}
+    <Container>
+      <Typography variant="h3" align="center" color="textSecondary">
+        Nida Pervez
+      </Typography>
+      {/* component="./Pictures/nidpic.JPG" */}
+      {/* <Box alignItems="center" borderRadius="50%"{...defaultProps}/> */}
+      <Container  >
+      <Box 
+        mt={5}
+        mb={5}
+        mx="auto"
+        display="flex" 
+        width={80} height={80} 
+        bgcolor="lightgreen"
+        alignItems="center"
+        justifyContent="center"
+        borderRadius="50%"{...defaultProps}
+        // l="50%"
+        // component="./Pictures/nidpic.JPG"
+        // style={{justifyContent: 'center'}}
+      >Hello</Box></Container>
+      <GridList cellHeight={300}  cols={3}>
+        {pictures.map(pic => (
+          <GridListTile key={pic.id} cols={pic.cols || 1}>
+            <img src={pic.img}  /> 
+            {/* //alt={tile.title} */}
+          </GridListTile>
+        ))}
+      </GridList>
+
+      <Button variant="contained" color="primary">
+      Hello World
+      </Button>
+    </Container>
   );
-}
+};
 
 export default App;
