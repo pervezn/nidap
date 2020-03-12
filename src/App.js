512e8e06580d5def373107cc7f6fdf66751b3fc3
pervezn <nidapervez2020@u.northwestern.edu>
2020-03-12 10:26:51 -0500

    app.js

--- src/App.js
+++ src/App.js
@@ -11,25 +11,35 @@ import FactFlow from "./FactFlow";
 import TropeTracker from "./TropeTracker";
 import Internships from "./Internships"
 import {Route, Link} from 'react-router-dom';
-
+import LalaLand from './LLLHome';
+import Movies from './LLLMovies';
+import Photos from './LLLPhotos';
+import Writing from './LLLWriting';
+import Travel from './LLLTravel';
+ 
 const defaultProps = {
-  style: { width: '10rem', height: '10rem' },
+ style: { width: '10rem', height: '10rem' },
 };
-
-
+ 
+ 
 const App = ()  => {
-
-  return (
-    <div className="App">
-      <Route exact path="/" component={Home} />
-      <Route exact path="/home" component={Home} />
-      <Route exact path="/about" component={About} />
-      <Route exact path="/projects/factflow" component={FactFlow} />
-      <Route exact path="/projects/tropetracker" component={TropeTracker} />
-      <Route exact path="/projects/internships" component={Internships} />
-    </div>
-    
-  );
+ 
+ return (
+   <div className="App">
+     <Route exact path="/" component={Home} />
+     <Route exact path="/home" component={Home} />
+     <Route exact path="/about" component={About} />
+     <Route exact path="/projects/factflow" component={FactFlow} />
+     <Route exact path="/projects/tropetracker" component={TropeTracker} />
+     <Route exact path="/projects/internships" component={Internships} />
+     <Route exact path="/LalaLand" component={LalaLand} />
+     <Route exact path="/LalaLand/Movies" component={Movies} />
+     <Route exact path="/LalaLand/Photos" component={Photos} />
+     <Route exact path="/LalaLand/Writing" component={Writing} />
+     <Route exact path="/LalaLand/Travel" component={Travel} />
+   </div>
+  
+ );
 };
-
+ 
 export default App;
