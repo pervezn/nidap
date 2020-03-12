6927ead28172f2b41c612506926eae8257db0ec5
pervezn <nidapervez2020@u.northwestern.edu>
2020-03-12 10:26:08 -0500

    about.js

--- src/About.js
+++ src/About.js
@@ -4,114 +4,124 @@ import './App.css';
 import { makeStyles } from '@material-ui/styles';
 import { Paper, Image, Avatar, Typography, Box, Grid, GridListTile, GridList, Button, Card, CardMedia } from '@material-ui/core';
 import {Route, Link} from 'react-router-dom';
-
+ 
 const useStyles = makeStyles({
-    paperMaster: {
-      // padding: "2vw",
-      textAlign: "center",
-      color: "#000000",
-      // background: "#A0DB9E",
-      // border: "5px solid black",
-    },
-    paper0: {
-      height:"100vh",
-      width: "3vw",
-      background: "#55769A",
-    },
-    paper1: {
-      height:"100%",
-      width: "40vw",
-      borderLeft: "25px solid #55769A",
-      borderRadius: "0px",
-      // backgroundColor: "blue",
-      // height:"80vh",
-      // width: "37vw",
-      // position: "-webkit-sticky", 
-      // position: "sticky", 
-      // top: "0"
-    },
-    paper2: {
-      height:"80vh",
-      width: "60vw",
-    },
-    paper3: {
-      height:"40vh",
-      width: "50vw",
-      // background: "#55769A",
-      border: "1px solid #55769A",
-      flexDirection: "row",
-      display: "flex",
-      
-    },
-    paper4:{
-      height:"65vh",
-      width: "27vw",
-      // background: "#55769A",
-      paddingTop: "10vw", 
-      marginLeft: "10vw",
-      display: "sticky",
-      textAlign: "left",
-    },
-    paper5:{
-      // height:"65vh",
-      width: "10vw",
-      // height: "40vh",
-      // background: "#55769A"
-    },
-    paper6:{
-      // height:"65vh",
-      width: "40vw",
-      height: "40vh",
-      // background: "#55769A"
-    },
-    button1: {
-      display: "inline-block",
-      color: "#49BEAA",
-    },
-    button2: {
-      marginBottom: "2vw", 
-      marginLeft: "5vw"
-    },
-    avatar1: {
-      width: 300,
-      height: 300,
-      marginTop: "5vw"
-    },
-  })
-
-
-
-
+   paperMaster: {
+     // padding: "2vw",
+     textAlign: "center",
+     color: "#000000",
+     // background: "#A0DB9E",
+     // border: "5px solid black",
+   },
+   paper0: {
+     height:"100vh",
+     width: "3vw",
+     background: "#55769A",
+   },
+   paper1: {
+     height:"100%",
+     width: "37vw",
+     // borderLeft: "25px solid #55769A",
+     // borderRadius: "0px",
+     // backgroundColor: "blue",
+     // height:"80vh",
+     // width: "37vw",
+     // position: "-webkit-sticky",
+     // position: "sticky",
+     // top: "0"
+   },
+   paper2: {
+     height:"80vh",
+     width: "60vw",
+   },
+   paper3: {
+     height:"40vh",
+     width: "50vw",
+     // background: "#55769A",
+     border: "1px solid #55769A",
+     flexDirection: "row",
+     display: "flex",
+    
+   },
+   paper4:{
+     height:"65vh",
+     width: "27vw",
+     // background: "#55769A",
+     paddingTop: "10vw",
+     marginLeft: "10vw",
+     display: "sticky",
+     textAlign: "left",
+   },
+   paper5:{
+     // height:"65vh",
+     width: "10vw",
+     // height: "40vh",
+     // background: "#55769A"
+   },
+   paper6:{
+     // height:"65vh",
+     width: "40vw",
+     height: "40vh",
+     // background: "#55769A"
+   },
+   button1: {
+     display: "inline-block",
+     color: "#49BEAA",
+   },
+   button2: {
+     marginBottom: "2vw",
+     marginLeft: "5vw"
+   },
+   avatar1: {
+     width: 300,
+     height: 300,
+     marginTop: "5vw"
+   },
+ })
+ 
+ 
+ 
+ 
 const About = ()  => {
-    const classes = useStyles();
-    return (
-      <div className="About">
-        <Grid container >
-        {/* style={{backgroundColor: "purple"}} */}
-            {/* <Paper elevation={0} square className={classes.paper0}></Paper> */}
-            <Grid item>
-            <Paper elevation={0} className={[classes.paper1, classes.paperMaster]}>
-                <Paper elevation={0} className={classes.paper4} style={{position: "-webkit-sticky", position: "sticky", top: "0"}}>
-                <Button className={classes.button1}>
-                    <Link style={{color: "#49BEAA", textDecoration: "none"}} to="/">Home</Link>
-                </Button>
-                <Button className={classes.button1}>
-                    <Link style={{color: "#49BEAA", textDecoration: "none"}} to="/about">About</Link>
-                </Button>
-                    <Button className={classes.button1}>Resume</Button>
-                    <Avatar src="../Pictures/nidpic.JPG" className={classes.avatar1}/>
-                </Paper>
-            </Paper>
-            </Grid>
-            <Grid item>
-                <Paper elevation={0} className={classes.paper2}>
-                    <Typography style={{marginTop: "10vw", textAlign: "left"}} variant="h1">Hello World.</Typography>
-                </Paper>
-            </Grid>
-        </Grid>
-      </div>
+   const classes = useStyles();
+   return (
+     <div className="About">
+       <Grid container style={{ borderLeft: "25px solid #55769A",}}>
+       {/* style={{backgroundColor: "purple"}} */}
+           {/* <Paper elevation={0} square className={classes.paper0}></Paper> */}
+           <Grid item>
+           <Paper elevation={0} className={[classes.paper1, classes.paperMaster]}>
+               <Paper elevation={0} className={classes.paper4} style={{position: "-webkit-sticky", position: "sticky", top: "0"}}>
+               <Button className={classes.button1}>
+                   <Link style={{color: "#49BEAA", textDecoration: "none"}} to="/">Home</Link>
+               </Button>
+               <Button className={classes.button1}>
+                   <Link style={{color: "#49BEAA", textDecoration: "none"}} to="/about">About</Link>
+               </Button>
+                   <Button className={classes.button1}>Resume</Button>
+                   <Avatar src="../Pictures/nidpic.JPG" className={classes.avatar1}/>
+               </Paper>
+           </Paper>
+           </Grid>
+          
+           <Grid item>
+               <Paper elevation={0} className={classes.paper2}>
+                   <Typography style={{marginTop: "10vw", textAlign: "left"}} variant="h1">Hello World.</Typography>
+               </Paper>
+           </Grid>
+ 
+           <Paper elevation={0} style={{display: "inline-block", width: "100%", marginTop: "5vh"}}>
+             <div style={{float: "right", width: "50%"}}>
+             <Button style={{color: "#55769A"}}>LinkedIn</Button>
+             <Button style={{color: "#55769A"}}>Email</Button>
+             <Button style={{color: "#55769A"}}>Github</Button>
+             <Button><Link style={{color: "#55769A", textDecoration: "none"}} to="/LalaLand">Lala-Land</Link></Button>
+           </div>
+         </Paper>
+       </Grid>
       
-    );
-  };
-  
+     </div>
+    
+   );
+ };
   export default About;
