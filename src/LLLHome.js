ff893677fd76a050e0fb033afdd79a2a2b27f762
pervezn <nidapervez2020@u.northwestern.edu>
2020-04-14 14:17:34 -0400

    home blurb change

--- src/LLLHome.js
+++ src/LLLHome.js
@@ -33,9 +33,9 @@ const LalaLand = ()  => {
        <Paper elevation={0} className={classes.paper1}>
            <Typography variant="h3" style={{color: "#EEB868", textAlign: "center"}}>Lala-Land</Typography>
            <Avatar alt="Nida P" src="../Pictures/nidpic.JPG" className={classes.avatar1}/>
-           <Typography variant="body1" style={{width: "30vw", marginLeft: "auto", marginRight:"auto", color: "#EEB868", marginTop: "5vh", textAlign: "center"}}>Hello World. Welcome to my Lala-Land where I am unrestrictedly and unapologetically me. Find out more about me through my many other hobbies. I hope to connect with you at some level !</Typography>
+           <Typography variant="body1" style={{width: "30vw", marginLeft: "auto", marginRight:"auto", color: "#EEB868", marginTop: "5vh", textAlign: "center"}}>Hey friends! My name’s Nida Pervez. I’m currently wrapping up my final year at Northwestern University, studying Computer Science and Chinese. Check out my past projects, internships, and creative ventures!</Typography>
        </Paper>
-       <div style={{display: "inline-box", marginTop: "15vh", width: "100%"}}>
+       <div style={{display: "inline-box", marginTop: "10vh", width: "100%"}}>
           <Button style={{ fontSize: "large"}}><Link style={{color: "#55769A", textDecoration: "none"}} to="/projects">Projects</Link></Button>
           <Button style={{color: "#55769A", fontSize: "large", marginLeft: "3vw"}}><Link style={{color: "#55769A", textDecoration: "none"}} to="/about">About</Link></Button>
           <Button style={{color: "#55769A", fontSize: "large", marginLeft: "3vw"}}>Resume</Button>
