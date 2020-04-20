bfb2f17051fed4bcb05b3192ea9cd93a80cacdb7
pervezn <nidapervez2020@u.northwestern.edu>
2020-04-20 13:23:44 -0400

    internships page

--- src/Internships.js
+++ src/Internships.js
@@ -45,11 +45,12 @@ paper2: {
     
   },
  paper3: {
-   height:"40vh",
+   height:"20vh",
    width: "50vw",
   //  border: "1px solid #55769A",
    flexDirection: "row",
    display: "flex",
+  //  backgroundColor: "green",
    
  },
   paper4: {
@@ -73,9 +74,10 @@ paper2: {
    display:"inline-block",
    textAlign: "left",
    color: "#55769A",
+   backgroundColor: "red",
  },
   paper8: {
-     width: "60vw",
+    //  width: "100%",
      textAlign: "left",
      marginTop: "4vw",
      marginLeft: "10vw",
@@ -122,29 +124,33 @@ const Internships = ()  => {
                </Grid>
               <Grid item style={{marginLeft: "20vw"}}>
                 <Paper elevation={0} className={classes.paper2}>
-                  <Paper elevation={0} className={classes.paper7} style={{width: "30vw"}}>
-                  <Paper elevation={0} className={classes.paper3}>
-                    <Paper elevation={0} className={classes.paper4} style={{width: "15vw",textAlign:"center"}}>
-                        {/* <img src={"../Pictures/knightLab.jpg"} style={{height:"100%",  alignContent:"right"}}/> */}
+                  <Paper elevation={0} className={classes.paper7} style={{width: "40vw"}}>
+                    <Paper elevation={0} className={classes.paper3}>
+                      {/* <Paper elevation={0} className={classes.paper4} style={{width: "15vw",textAlign:"center"}}>
+                          <img src={"../Pictures/knightLab.jpg"} style={{height:"100%",  alignContent:"right"}}/>
+                      </Paper> */}
+                      <Paper elevation={0} className={classes.paper4} style={{width: "35vw"}}>
+                          <Typography variant="h3" style={{marginTop: "0px", paddingTop: "0px", color: "#EF767A"}}>Internships</Typography>
+                      </Paper>
+                    
                     </Paper>
-                    <Paper elevation={0} className={classes.paper4} style={{width: "35vw"}}>
-                        <Typography variant="h3" style={{marginTop: "0px", paddingTop: "0px", color: "#EF767A"}}>Internships</Typography>
-                    </Paper>
-                   
-                    <Paper elevation={0} className={classes.paper8}></Paper>
-                  </Paper>
                   </Paper >
                   <Paper elevation={0} className={classes.paper8}>
-
-                  <Paper elevation={0} className={classes.paper7} style={{width: "11vw", }}>
-                        APPLE
-                      
-                    </Paper>
-                    <Paper elevation={0} className={classes.paper7} style={{width: "30vw"}}>
-                        ALLSTATE
-                    </Paper >
-                      <Typography variant="h6" style={{marginTop: "2vw"}}> My Role</Typography>
-                      <Typography variant="h6" style={{marginTop: "2vw"}}> Design Process</Typography>
+                    <Typography variant="h6" style={{marginTop: "2vw"}}>Summer 2019: <Link style={{color: "#EF767A", textDecoration: "none"}}>Apple</Link></Typography>
+                    <Typography variant="body1" style={{marginTop: "2vw"}}>
+                      I worked at Apple the summer of 2019 as a software engineer intern on the Apple Online Store Front-end team. At Apple I created a proof of concept collaborative shopping feature for the Apple Online Store. To Complete this project, I used NodeJS, Express, and node-fetch API to connect and communicate to the server. Furthermore, I used jQuery, HTML, CSS, and Javascript to create interactive components. For more information, please contact me at nidapervez2020@u.northwestern.edu.
+                    </Typography>  
+                    
+                    <Typography variant="h6" style={{marginTop: "5vw"}}>Summer 201:  <Link style={{color: "#EF767A", textDecoration: "none"}}>Allstate</Link></Typography> 
+                    <Typography variant="body1" style={{marginTop: "2vw"}}>
+                        I worked at the home office of Allstate the summer of 2018 as a software engineer intern. There, I worked on two main projects: a content management system and an Editable Quote. 
+                    </Typography> 
+                    <Typography variant="body1" style={{marginTop: "2vw"}}>
+                        The content management system I worked on was a web application that made accessing and editing information from the database easier and more efficient for users. To complete this project, I used asp.NET MVC framework and HTML for the front end, Oracle Database for the backend, and C# to connect the two. Implements a grid to display the information.
+                    </Typography>   
+                    <Typography variant="body1" style={{marginTop: "2vw"}}> 
+                        I spent the majority of my time working on the Editable Quote. Editable Quote is an edit feature for proposals that are sent out to customers, which allows users to streamline the quote process by seeing their quotes update in real time as different options are selected. By allowing users to edit their own quotes, they would bypass the ping pong communication that happens between insurance agents. This adaptation projected to increase bind rates by 20%. To complete this project, I used C#, JavaScript, Html, and CSS to implement the front end, and an XML file to read and write to the backend. 
+                    </Typography> 
                   </Paper>
                 </Paper>
               </Grid>
