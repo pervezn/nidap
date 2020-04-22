dbbf942345fc9fd7aefa9d8d86de34438501265e
pervezn <nidapervez2020@u.northwestern.edu>
2020-04-22 15:41:00 -0400

    addded icons to Home page

--- src/Home.js
+++ src/Home.js
@@ -23,6 +23,12 @@ const useStyles = makeStyles({
    },
    Typography:{
        fontStyle: "Lucida Grande"
+   },
+   button1: {
+     width: "15vw",
+     height: "15vh",
+     color: "#55769A", 
+     fontSize: "large"
    }
  
 })
@@ -30,24 +36,68 @@ const Home = ()  => {
    const classes = useStyles();
    return (
      <div className="Home">
-       <Paper elevation={0} className={classes.paper1}>
-           <Typography variant="h3" style={{color: "#EEB868", textAlign: "center"}}>Lala-Land</Typography>
+       <Paper elevation={0} className={classes.paper1} style={{marginTop: "15vh"}}>
+           {/* <Typography variant="h3" style={{color: "#EEB868", textAlign: "center"}}>Lala-Land</Typography> */}
            <Avatar alt="Nida P" src="../Pictures/nidpic.JPG" className={classes.avatar1} />
-           {/* style={{marginTop: "15vh"}} */}
+           
            <Typography variant="body1" style={{width: "30vw", marginLeft: "auto", marginRight:"auto", color: "#EEB868", marginTop: "5vh", textAlign: "center"}}>Hey friends! My name’s Nida Pervez. I’m currently wrapping up my final year at Northwestern University, studying Computer Science and Chinese. Check out my past projects, internships, and creative ventures! I hope to connect with you at some level :)</Typography>
        </Paper>
-       <div style={{display: "inline-box", marginTop: "10vh", width: "100%"}}>
-          <Button style={{ fontSize: "large"}}><Link style={{color: "#55769A", textDecoration: "none"}} to="/projects">Projects</Link></Button>
-          <Button style={{color: "#55769A", fontSize: "large", marginLeft: "3vw"}}><Link style={{color: "#55769A", textDecoration: "none"}} to="/about">About</Link></Button>
-          <Button style={{color: "#55769A", fontSize: "large", marginLeft: "3vw"}}>Resume</Button>
+       <div style={{display: "inline-box", marginTop: "15vh", width: "100%",}}>
+
+        <Button className={classes.button1}>
+          <Link style={{color: "#55769A", textDecoration: "none"}} to="/projects">
+            <img src={"../Pictures/ProjectIcon.png"} style={{width:"50%"}}/>
+            <Typography>Projects</Typography>
+          </Link>
+        </Button>
+
+        <Button className={classes.button1} >
+          <Link style={{color: "#55769A", textDecoration: "none"}} to="/about">
+            <img src={"../Pictures/AboutMeIcon.png"} style={{width:"40%"}}/>
+            <Typography>About</Typography>
+          </Link>
+        </Button>
+
+        <Button className={classes.button1} >
+          <Link style={{color: "#55769A", textDecoration: "none"}} to="/">
+            <img src={"../Pictures/ResumeIcon.png"} style={{width:"30%"}}/>
+            <Typography>Resume</Typography>
+          </Link>
+        </Button>
+
        </div>
-       <div style={{display: "inline-box", marginTop: "5vh", width: "100%"}}>
-           {/* <div style={{float: "right", width: "50%"}}> */}
-           <Button style={{ fontSize: "large"}}><Link style={{color: "#55769A", textDecoration: "none"}} to="/Movies">Movies</Link></Button>
-           <Button style={{color: "#55769A", fontSize: "large", marginLeft: "3vw"}}><Link style={{color: "#55769A", textDecoration: "none"}} to="/Photos">Photos</Link></Button>
-           <Button style={{color: "#55769A", fontSize: "large", marginLeft: "3vw"}}><Link style={{color: "#55769A", textDecoration: "none"}} to="/Writing">Writing</Link></Button>
-           <Button style={{color: "#55769A", fontSize: "large", marginLeft: "3vw"}}><Link style={{color: "#55769A", textDecoration: "none"}} to="/Travel">Travel</Link></Button>
-           {/* </div> */}
+       <div style={{display: "inline-box", marginTop: "7vh", width: "100%"}}>
+    
+           <Button className={classes.button1}>
+             <Link style={{color: "#55769A", textDecoration: "none"}} to="/Movies">
+             <img src={"../Pictures/MoviesIcon.png"} style={{width:"40%"}}/>
+               <Typography>Movies</Typography>
+              </Link>
+          </Button>
+
+          <Button className={classes.button1}>
+            <Link style={{color: "#55769A", textDecoration: "none"}} to="/Photos">
+            <img src={"../Pictures/PhotosIcon.png"} style={{width:"50%"}}/>
+            <Typography>Photos</Typography>
+            </Link>
+          </Button>
+
+          <Button className={classes.button1}>
+            <Link style={{color: "#55769A", textDecoration: "none"}} to="/Writing">
+            <img src={"../Pictures/WritingIcon.png"} style={{width:"30%"}}/>
+            <Typography>Writing</Typography>
+            </Link>
+          </Button>
+           
+       </div>
+       <div style={{display: "inline-box", marginTop: "3vh", width: "100%", marginBottom: "7vh"}}>
+          <Button className={classes.button1}>
+            <Link style={{color: "#55769A", textDecoration: "none"}} to="/Travel">
+            <img src={"../Pictures/TravelIcon.png"} style={{width:"40%"}}/>
+            <Typography>Travel</Typography>
+              
+            </Link>
+          </Button>
        </div>
      </div>
     
